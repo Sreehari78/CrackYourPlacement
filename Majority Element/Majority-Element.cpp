472ddca3b6d639297aa988e24214e01c30193364
Sreehari S <shraxx7@gmail.com>
2024-07-17 21:36:52 +0530

    Create Majority-Element.cpp

---
+++ Majority Element/Majority-Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, candidate = -1;
+
+        for(int it: nums) 
+            if(it == candidate) count++;
+            else if(count == 0) candidate = it;
+            else count--;
+
+        return candidate;
+    }
+};
