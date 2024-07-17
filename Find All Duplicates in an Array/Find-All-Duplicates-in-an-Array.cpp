1d2be8421dd69850aab47ef7e12d115aa5ecdba5
Sreehari S <shraxx7@gmail.com>
2024-07-17 22:25:38 +0530

    Create Find-All-Duplicates-in-an-Array.cpp

---
+++ Find All Duplicates in an Array/Find-All-Duplicates-in-an-Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        std::cout.tie(nullptr);
+
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++)
+            nums[(nums[i] - 1) % nums.size()] += nums.size();
+
+        for (int i = 0; i < nums.size(); i++)
+            if ((nums[i] - 1) / nums.size() > 1)
+                res.push_back(i + 1);
+
+        return res;
+    }
+};
